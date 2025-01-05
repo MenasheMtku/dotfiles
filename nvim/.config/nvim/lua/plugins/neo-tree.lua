b84ac8cdc039ae2263fa4f7e0b2e834382158f2a
MenasheMtku <menashemetuko@gmail.com>
2025-01-05 14:56:52 +0200

    show hidden filles by default

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,19 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,          -- Show hidden files by default
+          hide_dotfiles = false,   -- Ensure dotfiles are visible
+          hide_gitignored = false, -- Show files ignored by .gitignore
+          never_show = {           -- Ensure specific folders/files are never hidden
+            ".DS_Store",
+            "thumbs.db",
+          },
+        },
+      },
+    })
     -- Keymaps
     vim.keymap.set("n", "<C-a>", ":Neotree filesystem reveal toggle left<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal toggle float<CR>", {})
